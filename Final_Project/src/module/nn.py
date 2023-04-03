gradient fix

Final_Project/src/module/nn.py
@@ -181,8 +181,9 @@ class FeedForward:
             self.history['accuracy'].append(np.mean(np.equal(binary_label, predict_label)))
             self.history['loss'].append(np.mean(np.abs(binary_label - predict_label) * np.abs(scores)))
 
-            dout = 2 * (predict_label - binary_label) * np.abs(predict_label - binary_label)[:, np.newaxis] * np.sign(
+            dout = 2 * (predict_label - binary_label)[:, np.newaxis] * np.abs(predict_label - binary_label) * np.sign(
                 relu)
+
             dout = self.relu.backward(dout)
             config = self.linear.auto_grad(dout, config)
 